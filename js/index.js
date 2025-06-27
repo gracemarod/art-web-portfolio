ad0a03126469c0cccd83f887674576ec2085893d
Grace M Rodriguez Gomez <gracemarierodriguez6@gmail.com>
2025-06-27 13:49:09 -0400

    Updating home page.

--- js/index.js
+++ js/index.js
@@ -169,7 +169,7 @@ $(function(){
       {src: 'Mori_Vivi_Forest_Pipes_Rendered.jpg', srct: 'thumbnails/tn_Mori_Vivi_Forest_Pipes_Rendered.jpg', description:'#digital', title:'The Hidden Pipes in Morivivi\'s Forest'},
       {src: 'At_the_High_Line_Observation_Deck.jpeg', srct:'thumbnails/tn_At_the_High_Line_Observation_Deck.jpeg', description: '#traditional', title:'At the High Line Observation Deck', },
       {src: 'Life_Drawing2.jpeg', srct: 'thumbnails/tn_Life_Drawing2.jpeg', description: '#figurative', title:'Long Pose Figure Drawing Study with Pencil'},
-      {src: 'Galaxy_Dreams.jpg', srct:'thumbnails/tn_Galaxy_Dreams.jpg', description: '#traditional', title: 'Galaxy Dreams'},
+      {src: 'Galaxy_Dreams.jpeg', srct:'thumbnails/tn_Galaxy_Dreams.jpg', description: '#traditional', title: 'Galaxy Dreams'},
       {src: 'Black_Elf.jpeg', srct: 'thumbnails/tn_Black_Elf.jpeg', description:'#digital', title: 'High Fantasy Black Elf' },
       {src: 'LifeDrawing_ColoredPencils.jpeg', srct:'thumbnails/tn_LifeDrawing_ColoredPencils.jpeg' , description: '#figurative', title: 'Long Pose Figure Drawing Study with Colored Pencils' },
       {src: 'Master_studies_ghibli_background.png', srct:'thumbnails/tn_Master_studies_ghibli_background.png', description: '#digital', title:'Master Study of Ghibli\'s Vegetation'},
